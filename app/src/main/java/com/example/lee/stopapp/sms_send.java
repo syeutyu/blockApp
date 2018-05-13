411c3ed1e34cec145569124c0a6a0d15746b1e8f
syeutyu <syeuty@naver.com>
2018-05-13 12:46:06 +0900

    [Add] 문자 전송, 정보 입력 Info 화면 생성

---
+++ app/src/main/java/com/example/lee/stopapp/sms_send.java
@@ -0,0 +1,13 @@
+package com.example.lee.stopapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sms_send extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sms_send);
+    }
+}
